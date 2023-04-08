ad2a207fc45a413413a765b35eb51cbc39a53b08
mohamedis8890 <mohamed.is8890@gmail.com>
2023-04-08 16:38:51 +0200

    Main Header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,17 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Header() {
   return (
-    <header>
+    <header className="flex justify-between p-5 bg-white backdrop-blur-md shadow-md sticky z-50">
       {/* left */}
-      <div>
+      <div className="flex space-x-2 items-center">
+        <Image
+          src="https://links.papareact.com/4t3"
+          width={30}
+          height={30}
+          alt="logo"
+        />
         <div>
-          <Image
-            src="https://links.papareact.com/4t3"
-            width={30}
-            height={30}
-            alt="logo"
-          />
           <h1 className="font-bold">
             The MoDev <span className="text-violet-400">AI</span> Image
             Generator
@@ -22,6 +23,22 @@ function Header() {
         </div>
       </div>
       {/* right */}
+      <div className="flex items-center text-xs md:text-base divide-x text-gray-500">
+        <Link
+          className="px-2 font-light text-right"
+          href="https://www.modev.guru/"
+          target="_blank"
+        >
+          Check out my other projects
+        </Link>
+        <Link
+          className="px-2 font-light"
+          href="https://github.com/mohamedis8890/ai-image-generator"
+          target="_blank"
+        >
+          Github Repo
+        </Link>
+      </div>
     </header>
   );
 }
