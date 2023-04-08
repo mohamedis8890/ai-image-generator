c0b72122b47f0f02fe81c1970f1693106b231aa0
mohamedis8890 <mohamed.is8890@gmail.com>
2023-04-09 00:37:02 +0200

    Setup GPT prompt generation

--- app/api/suggestion/route.ts
+++ app/api/suggestion/route.ts
@@ -1,9 +1,12 @@
 export async function GET(request: Request) {
   let textData = "";
   try {
-    const response = await fetch("...", {
-      cache: "no-store",
-    });
+    const response = await fetch(
+      "http://localhost:7071/api/getChatGPTSuggestion",
+      {
+        cache: "no-store",
+      }
+    );
     textData = await response.text();
   } catch (error) {
     console.error(error);
